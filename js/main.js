4719ff2cc2b15bccec122642eb63eb412690f46e
PawanJS <contactpawanjs@gmail.com>
2021-02-14 06:49:20 +0530

    Added DRY principle

---
+++ js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const sliders = document.querySelector(".js-slider");
+const slide = document.querySelectorAll(".js-slide");
+const btnPrev = document.querySelector(".js-btn-prev");
+const btnNext = document.querySelector(".js-btn-next");
